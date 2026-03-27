1706a3ecdd341ca902531039abeeb94f8916b45f
iveman99 <info.veman99@gmail.com>
2026-03-27 16:06:25 +0530

    phots all

--- leadershipData.js
+++ leadershipData.js
@@ -9,33 +9,33 @@
 const LEADERSHIP_DATA = {
     // Row 1: Core Executive Board
     coreExecutives: [
-        { name: "Yeman Adep", role: "President", photo: "./Photos/yeman.jpg", elite: true },
-        { name: "Vinod Siricilla", role: "Leo Advisor", photo: "./Photos/anna.jpg" },
-        { name: "Rahul Dudam", role: "IPP", photo: "./Photos/rahul.jpg" },
-        { name: "Veman Chippa", role: "Secretary", photo: "./Photos/veman.jpg" },
-        { name: "Varsha Siricilla", role: "Treasurer", photo: "./Photos/varsha.jpg" },
-        { name: "Namra Gada", role: "VP I", photo: "./Photos/namra.jpg" },
-        { name: "Manasa Samala", role: "VP II", photo: "./Photos/Manasa.jpg" }
+        { name: "Leo Yeman Adep", role: "President", photo: "./Photos/yeman.jpg", elite: true },
+        { name: "Lion Vinod Siricilla", role: "Leo Advisor", photo: "./Photos/anna.jpg" },
+        { name: "Leo Rahul Dudam", role: "IPP", photo: "./Photos/rahul.jpg" },
+        { name: "Leo Veman Chippa", role: "Secretary", photo: "./Photos/veman.jpg" },
+        { name: "Leo Varsha Siricilla", role: "Treasurer", photo: "./Photos/varsha.jpg" },
+        { name: "Leo Namra Gada", role: "VP I", photo: "./Photos/namra.jpg" },
+        { name: "Leo Manasa Samala", role: "VP II", photo: "./Photos/Manasa.jpg" }
     ],
 
     // Row 2: Team Leads & Directors
     teamLeads: [
-        { name: "Dhriti Shetty", role: "Joint Secretary", photo: "./Photos/dhriti.jpg" },
-        { name: "Omshankar Nakka", role: "Joint Secretary", photo: "./Photos/om.jpg" },
-        { name: "Shrikant Mergud", role: "Joint Treasurer", photo: "./Photos/shrikant.jpg" },
-        { name: "Nilesh Jaywant", role: "Club Mentor", photo: "./Photos/dada.jpg" },
-        { name: "Saloni Jarol", role: "GST Head", photo: "./Photos/saloni.jpg" },
-        { name: "Roshni Patel", role: "GMT Head", photo: "" },
-        { name: "Sonia Andavrapu", role: "GLT Head", photo: "" },
-        { name: "Srikant Yelle", role: "PRO", photo: "./Photos/shrikant.jpg" },
-        { name: "Yuvraj Rai", role: "Social Media Head", photo: "./Photos/Yuvraj.jpg" },
-        { name: "Gopi Darji", role: "Creative Head", photo: "./Photos/gopi.jpg" },
-        { name: "Abhilash Jejela", role: "Twinning Dir", photo: "./Photos/abhilash.jpg" },
-        { name: "Rushikesh Kamurthi", role: "Fellowship Dir", photo: "./Photos/rushi.jpg" },
-        { name: "Rakesh Kundan", role: "Sports Head", photo: "./Photos/rakesh.jpg" },
-        { name: "Sneha Adep", role: "Director", photo: "./Photos/sneha.jpg" },
-        { name: "Vivek Butla", role: "Director", photo: "./Photos/vivek.jpg" },
-        { name: "Jaya Adep", role: "Tail Twister", photo: "./Photos/jaya.jpg" },
-        { name: "Akshaya Vadigoppula", role: "Tamer", photo: "./Photos/Akshaya.jpg" },
+        { name: "Leo Dhriti Shetty", role: "Joint Secretary", photo: "./Photos/dhriti.jpg" },
+        { name: "Leo Omshankar Nakka", role: "Joint Secretary", photo: "./Photos/om.jpg" },
+        // { name: "Leo Shrikant Mergud", role: "Joint Treasurer", photo: "./Photos/shrikant.jpg" },
+        { name: "Lion Nilesh Jaywant", role: "Club Mentor", photo: "./Photos/dada.jpg" },
+        { name: "Leo Saloni Jaroli", role: "GST Head", photo: "./Photos/saloni.jpg" },
+        { name: "Leo Roshni Adep", role: "GMT Head", photo: "./Photos/roshni.jpg" },
+        { name: "Leo Sonia Andavrapu", role: "GLT Head", photo: "./Photos/sonia.jpg" },
+        { name: "Leo Srikant Yelle", role: "PRO", photo: "./Photos/shrikant.jpg" },
+        { name: "Leo Yuvraj Rai", role: "Social Media Head", photo: "./Photos/Yuvraj.jpg" },
+        { name: "Leo Gopi Darji", role: "Creative Head", photo: "./Photos/gopi.jpg" },
+        { name: "Leo Abhilash Jelje", role: "Twinning Dir", photo: "./Photos/abhilash.jpg" },
+        { name: "Leo Rushikesh Kamurthi", role: "Fellowship Dir", photo: "./Photos/rushi.jpg" },
+        { name: "Leo Rakesh Kundan", role: "Sports Head", photo: "./Photos/rakesh.jpg" },
+        { name: "Leo Sneha Adep", role: "Director", photo: "./Photos/sneha.jpg" },
+        { name: "Leo Vivek Butla", role: "Director", photo: "./Photos/vivek.jpg" },
+        { name: "Leo Jaya Adep", role: "Tail Twister", photo: "./Photos/jaya.jpg" },
+        { name: "Leo Akshaya Vadigoppula", role: "Tamer", photo: "./Photos/Akshaya.jpg" },
     ]
 };
